afcedc5c0619505a58faf9259009814f09ff9c80
MyShlongIsLongAT <florian_breuker@outlook.com>
2023-03-15 20:18:39 +0100

    working flashcards without item import

--- src/components/Home/FlashCard.js
+++ src/components/Home/FlashCard.js
@@ -1,27 +1,48 @@
 import React, {useEffect, useState} from 'react';
 import styles from './style.module.css';
+import {BsArrowLeftSquareFill, BsArrowRightSquareFill} from "react-icons/bs";
 
 function FlashCard(props) {
-    const [words] = useState(props.words);
-    const [curWord, setCurWord] = useState([]);
+    const [curPair, setCurPair] = useState([]);
+    const [curTask, setCurTask] = useState("");
+    const [curTaskId, setCurTaskId] = useState(0);
 
     useEffect(() => {
-        setCurWord(words[0]);
-    }, []);
-
-    console.log(curWord);
+        try {
+            setCurPair(props.words[curTaskId])
+            setCurTask(curPair.definition)
+        } catch (e) {
+            console.log("error")
+        }
+    }, [props, curTaskId, curPair]);
 
     return (
-        <div className={styles.wrapper} onClick={flipCard}>
-            <div className={styles.flashCard}>
-                <div>{curWord.term}</div>
+        <div className={styles.wrapper}>
+            <BsArrowLeftSquareFill size={70} color={"#96ADC8"} className={styles.flashCardArrows} onClick={() => {
+                setCurTaskId(prevTaskId => prevTaskId - 1);
+                if (curTaskId === 0) {
+                    setCurTaskId(0);
+                }
+            }
+            }/>
+            <div className={styles.flashCard} onClick={() => {
+                if (curTask === curPair.term) {
+                    setCurTask(curPair.definition);
+                } else {
+                    setCurTask(curPair.term);
+                }
+            }}>
+                {curTask}
             </div>
+            <BsArrowRightSquareFill size={70} color={"#96ADC8"} className={styles.flashCardArrows} onClick={() => {
+                setCurTaskId(prevTaskId => prevTaskId + 1);
+                if (curTaskId >= props.words.length - 1) {
+                    setCurTaskId(0);
+                }
+            }
+            }/>
         </div>
     );
 }
-
-let flipCard = () => {
-    console.log("lol")
-};
 
 export default FlashCard;
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -2,12 +2,16 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import WordList from "./WordList";
 import FlashCard from "./FlashCard";
+import {Dna} from "react-loader-spinner";
 
 function HomePage(props) {
     const [words, setWords] = useState([]);
+    const [loadingInProgress, setLoading] = useState(true);
+
+
     const instance = axios.create({
         baseURL: 'https://api.servker.cc/api/',
-        headers: {'Authorization': 'Bearer ' + 'd1b66a703a69e9bc9f63d5142bca339d768a7a49629bbbe6402eefa10368e715602d4f508c78329da9c852680616f1f3b2eccdcb9f24ccd2265e5bcd4e18290f4f5888607746600600febfe03382655179b9aeb96a4d77b19deaa12a5a351b1c3027a5e489a961e3a48fc5e340a0adf327166f291ccc26d0cbd3357b31b8a16e'}
+        headers: {'Authorization': 'Bearer ' + process.env.REACT_APP_API_KEY}
     });
 
     useEffect(() => {
@@ -16,18 +20,39 @@ function HomePage(props) {
                 let result = response.data.data;
                 setWords(sortWords(result));
             })
+        setLoading(false);
     }, []);
+
     return (
+
         <div>
-            <h1>Flash Card</h1>
-            <FlashCard words={words}/>
-            <WordList words={words}/>
+            {loadingInProgress ? (
+                <div>
+                    <Dna
+                        visible={true}
+                        height="80"
+                        width="80"
+                        ariaLabel="dna-loading"
+                        wrapperStyle={{}}
+                        wrapperClass="dna-wrapper"
+                    />
+                </div>
+
+            ) : (
+                <div>
+                    <h1>Flash Card</h1>
+                    <FlashCard words={words}/>
+                    <WordList words={words}/>
+                </div>
+
+            )}
         </div>
-    );
+    )
+        ;
 }
 
 let sortWords = (data) => {
-    let chosenDeck = data.filter(deck => deck.attributes.name === "test-2");
+    let chosenDeck = data.filter(deck => deck.attributes.name === "test");
     let chosenWords = chosenDeck[0].attributes.record;
     chosenWords = chosenWords.sort((a, b) => {
         if (a.term < b.term) {
@@ -38,7 +63,7 @@ let sortWords = (data) => {
         }
         return 0;
     });
-    return(chosenWords); //using cause setting the words wouldn't overwrite the whole thing... just adds an element
+    return (chosenWords); //using cause setting the words wouldn't overwrite the whole thing... just adds an element
 }
 
 export default HomePage;
